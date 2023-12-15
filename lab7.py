c5aed62a82ccdfaf8308d99932e4c4f40ec9d29c
Skribka-Filatova <uliafilatova2003@gmail.com>
2023-12-15 22:07:02 +0700

    Возврат отплаты

--- lab7.py
+++ lab7.py
@@ -8,3 +8,63 @@ def main():
     return render_template('lab7/index.html')
 
 
+@lab7.route('/lab7/drink')
+def drink():
+    return render_template('lab7/drink.html')
+
+
+@lab7.route('/lab7/api', methods = ['POST'])
+def api():
+    data = request.json
+
+    if data['method'] == 'get-price':
+        return get_price(data['params'])
+
+    if data['method'] == 'pay':
+        return pay(data['params'])
+
+    abort (400)
+
+def get_price(params):
+    return {"result": calculate_price(params), "error": None}
+
+def calculate_price(params):
+    drink= params['drink']
+    milk = params['milk']
+    sugar = params['sugar']
+
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if milk:
+        price += 30
+    if sugar:
+        price += 10
+        return price
+
+def pay(params):
+    card_num = params[ 'card_num ']
+    if len(card_num) !=16 or not card_num.isdigit():
+        return {"result": None, "error": "Неверный номер карты"}
+
+    cvv = params['cvv']
+    if len(cvv) != 10 or not cvv.isdigit():
+        return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+
+def pay(params):
+    card_num = params['card_num']
+    if len(card_num) != 16 or not card_num.isdigit():
+        return {"result": None, "error": "Неверный номер карты"}
+
+    cvv  = params['cvv']
+    if len(cvv) != 16 or not cvv.isdigit():
+        return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+    price = calculate_price(params)
+    return {"result" : f'C rfhns {card_num} списано {price} рублей'}
+        
