f0197523f3bbca95145b99cdac81a6e770a3fe23
Skribka-Filatova <uliafilatova2003@gmail.com>
2023-12-16 16:06:31 +0700

    Заготовка лб8

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
@@ -13,3 +14,4 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
